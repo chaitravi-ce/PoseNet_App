07a3e437bc30fd81e37e74c85b985fffa227fa6b
chaitravi-ce <chaitravi007@gmail.com>
2021-07-12 12:26:40 +0530

    Added Tflite

---
+++ python/video.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+
+interpreter = tf.lite.Interpreter(model_path="../assets/posenet_mv1_075_float_from_checkpoints.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+print(input_shape)
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
